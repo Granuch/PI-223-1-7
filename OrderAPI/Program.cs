5d1e6706c29eb8b2648e60614c034d7606ec8e1b
Yarickprogramist <hacker22860337@gmail.com>
2025-05-22 22:28:11 +0300

    Added some semblance of microservices

---
+++ OrderAPI/Program.cs
@@ -0,0 +1,40 @@
+using BLL.Interfaces;
+using BLL.Services;
+using Mapping.Mapping;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using PI_223_1_7.DbContext;
+using PI_223_1_7.Patterns.UnitOfWork;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+                options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LibratyDb;Trusted_Connection=True;"));
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V2");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
